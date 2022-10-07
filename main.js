ca3270dba3fc3fae5593daf3de69d5a5e1a84ec2
ReptilianZart <andreasjjhome@gmail.com>
2022-10-07 18:52:05 +0200

    test1

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const app = express();
-const port = process.env.PORT;
+const PORT = process.env.PORT;
 
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb' }))
 
 
-app.listen(port || 3000, function(req, res) {
-    console.log("listening on port "+port);
+app.listen(PORT || 3000, function(req, res) {
+    console.log("listening on port "+PORT);
 })
 
 app.get("/user", function(req, res) {
