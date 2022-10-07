6a8189bb651662eb2a51db042123526bd9f9a405
ReptilianZart <andreasjjhome@gmail.com>
2022-10-07 18:47:18 +0200

    added port and env

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 const express = require("express");
+const dotenv = require("dotenv").config();
 const app = express();
+const port = process.env.PORT;
 
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb' }))
 
-const port = 3000;
 
-app.listen(port, function(req, res) {
+app.listen(port || 3000, function(req, res) {
     console.log("listening on port "+port);
 })
 
